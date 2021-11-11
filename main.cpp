618583cbd0823a229014c63b90e8811e973d14ad
cosmobobak <cosmobobak@gmail.com>
2021-11-11 19:19:08 +0000

    most up to d8

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@
 #include <cmath>
 #include <iostream>
 #include <vector>
+#include <ranges>
+#include <numeric>
 
 auto palette(int i, int max_i) {
     static const auto characters = " .:-=+*#%@";
-    i = std::min(i, max_i);
-    i = std::max(i, 0);
+    i = std::clamp(i, 0, max_i);
     int index = (i * 9) / max_i;
     if (!(index < 10 && index >= 0)) {
         std::cout << index << "\n";
@@ -26,9 +27,10 @@ auto log_shift(int i) {
 }
 
 auto mandlebrot(size_t xdim, size_t ydim) {
-    // std::vector<std::vector<int>> counts(ydim, std::vector<int>(xdim));
 
     constexpr int max_iteration = 1000;
+    std::vector<int> iter_counts;
+    iter_counts.reserve(xdim * ydim);
 
     for (size_t ypos = 0; ypos < ydim; ypos++) {
         std::cout << "|";
@@ -46,20 +48,30 @@ auto mandlebrot(size_t xdim, size_t ydim) {
                 x = xtemp;
                 iteration++;
             }
+            iter_counts.push_back(iteration);
             char color = palette(log_shift(iteration) /*upward_shift(iteration, max_iteration)*/, max_iteration);
             // plot(Px, Py, color);
             std::cout << color;
         }
         std::cout << "|\n";
     }
+    std::ranges::sort(iter_counts);
+    auto max_count = *std::ranges::max_element(iter_counts);
+    auto min_count = *std::ranges::min_element(iter_counts);
+    auto avg_count = std::accumulate(iter_counts.begin(), iter_counts.end(), 0) / iter_counts.size();
+    auto median = iter_counts[iter_counts.size() / 2];
+    std::cout << "max: " << max_count << "\n";
+    std::cout << "min: " << min_count << "\n";
+    std::cout << "avg: " << avg_count << "\n";
+    std::cout << "med: " << median << "\n";
 }
 
 int main(int argc, char const *argv[]) {
     if (argc == 1) {
         mandlebrot(48, 16);
-        // mandlebrot(1200, 300); // biggest possible zoom on windows terminal
+        // mandlebrot(1200, 300); // biggest possible zoom on windows terminal 1200 300
     } else if (argc == 3) {
-        mandlebrot(atoi(argv[1]), atoi(argv[2]));
+        mandlebrot(atol(argv[1]), atol(argv[2]));
     }
     return 0;
 }
